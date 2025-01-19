aeefdc355feb8c920e7f5ebde8c0d5b43d8522f9
Esteban Kroh <ekroh@nubity.com>
2025-01-19 11:36:10 -0300

    Migrando a Svelte 5

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-	plugins: [tailwindcss(), autoprefixer()]
+export default {
+	plugins: {
+	  tailwindcss: {},
+	  autoprefixer: {}
+	}
 };
-
-export default config;
+  
